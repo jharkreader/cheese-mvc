1687672c5acb0e62d0e8fefc9cd054f7171bf437
Jennifer Harkreader <jen3072@gmail.com>
2017-08-20 18:40:52 -0700

    Added validation, enum class CheeseType

---
+++ src/main/java/org/launchcode/cheesemvc/models/CheeseType.java
@@ -0,0 +1,21 @@
+package org.launchcode.cheesemvc.models;
+
+import javax.swing.*;
+
+public enum CheeseType {
+
+    HARD ("Hard"),
+    SOFT ("Soft"),
+    FAKE ("Fake");
+
+    private final String name;
+
+    CheeseType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
