be526cb251b00be50f02676826042a3496c52095
Jennifer Harkreader <jen3072@gmail.com>
2017-08-23 20:31:33 -0700

    Added model validation to User class

--- src/main/java/org/launchcode/cheesemvc/controllers/UserController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/UserController.java
@@ -2,11 +2,15 @@ package org.launchcode.cheesemvc.controllers;
 
 
 import org.launchcode.cheesemvc.models.User;
+import org.launchcode.cheesemvc.models.UserData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("user")
@@ -16,22 +20,37 @@ public class UserController {
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String add(Model model) {
 
+        model.addAttribute(new User());
         return "/user/add";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String add(Model model, @ModelAttribute User user, String verify) {
+    public String add(Model model, @ModelAttribute @Valid User user, Errors errors, String verify) {
 
-        if (verify.equals(user.getPassword())) {
-            model.addAttribute("user", user);
-            return "/user/index";
+        if (errors.hasErrors()) {
+            model.addAttribute(user);
+            return "user/add";
+        }
+
+        if (user.getPassword() == null || verify == null) {
+            user.setPassword("");
+            model.addAttribute("passwordError", "Enter your password and verify it.");
+            model.addAttribute(user);
+            return "user/add";
+        }
+
+        if (!user.getPassword().equals(verify)) {
+            user.setPassword("");
+            model.addAttribute("passwordError", "Password and verification must match.");
+            model.addAttribute(user);
+            return "user/add";
         }
         else {
-            String errorMessage = "Please check that your passwords match.";
-            model.addAttribute("error", errorMessage);
-            return "/user/add";
-        }
+            UserData.add(user);
+            model.addAttribute(user);
+            return "user/index";
 
+        }
 
     }
 
--- src/main/java/org/launchcode/cheesemvc/models/User.java
+++ src/main/java/org/launchcode/cheesemvc/models/User.java
@@ -1,24 +1,41 @@
 package org.launchcode.cheesemvc.models;
 
+import org.hibernate.validator.constraints.Email;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 public class User {
 
+    @NotNull
+    @Size(min=5, max=15, message = "Username must be between 5 and 15 characters.")
     private String username;
+
+    @Email(message = "Please enter a valid email")
     private String email;
+
+    @NotNull
+    @Size(min=6, message = "Password must be at least 6 characters")
     private String password;
+
     private int userId;
     private Date joined;
     private static int nextUserId = 1;
 
 
-    public User(String aUsername, String anEmail, String aPassword) {
-        this.username = aUsername;
-        this.email = anEmail;
-        this.password = aPassword;
+    public User(String username, String email, String password) {
+        this();
+        this.username = username;
+        this.email = email;
+        this.password = password;
         this.userId = nextUserId;
         this.joined = new Date();
         nextUserId++;
+    }
+
+    public User() {
+
     }
 
     public int getUserId() {
